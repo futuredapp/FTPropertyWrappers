5ab318032f7d87dfdd78c4da576243fd575a1582
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-11-05 10:27:30 +0100

    Add separate stored subject tests

---
+++ Tests/FTPropertyWrappersTests/StoredSubjectsTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+import FTPropertyWrappers
+
+final class StoredSubjectTests: XCTestCase {
+    @StoredSubject var number: Int = 0
+
+    func testStoredSubjectObserving() {
+        var values: [Int] = []
+        let bag = DisposeBag()
+
+        _number.observe { old, new in
+            XCTAssertEqual(old, 0)
+            values.append(new)
+        }.dispose(in: bag)
+
+        _number.observe { value in
+            values.append(value)
+        }.dispose(in: bag)
+
+        number = 1
+
+        XCTAssertEqual(values, Array(repeating: 1, count: 2))
+    }
+
+    func testStoredSubjectDisposing() {
+        var values: [Int] = []
+        var bag = DisposeBag()
+
+        _number.observe { value in
+            values.append(value)
+        }.dispose(in: bag)
+
+        bag = DisposeBag()
+
+        number = 1
+
+        XCTAssertEqual(values, [])
+    }
+}
