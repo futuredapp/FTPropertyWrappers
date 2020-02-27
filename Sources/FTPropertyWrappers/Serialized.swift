9f31184a27beecf522bdc33228b75675a5b87c41
Mikoláš Stuchlík <mikolas.stuchlik@thefuntasty.com>
2020-02-27 11:40:01 +0100

    Apply pull request suggestions

--- Sources/FTPropertyWrappers/Serialized.swift
+++ Sources/FTPropertyWrappers/Serialized.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-@propertyWrapper
 /// This class provides user with easy way to serialize access to a property in multiplatform environment.
+@propertyWrapper
 public final class Serialized<Value> {
 
     /// Synchronization queue for the property. Read or write to the property must be perforimed on this queue
@@ -15,10 +15,13 @@ public final class Serialized<Value> {
     }
 
 
-    /// Did set observer for stored property. Notice, that didSet event is called on the synchronization queue. You should free this thread asap with async call, since complex operations would slow down sync access to the property.
+    /// Did set observer for stored property. Notice, that didSet event is called on the synchronization queue.
+    /// You should free this thread asap with async call, since complex operations would slow down sync access
+    /// to the property.
     public var didSet: ((Value) -> Void)?
 
-    /// Inserting initial value to the property. Notice, that this operation is NOT DONE on the synchronization queue. This initializer uses predefined label for dispatch queue.
+    /// Inserting initial value to the property. Notice, that this operation is NOT DONE on the synchronization queue.
+    ///  This initializer uses predefined label for dispatch queue.
     /// - Parameter wrappedValue: Default value, not inserted on synchronization queue
     public init(wrappedValue: Value) {
         queue = DispatchQueue(label: "app.futured.ftpropertywrappers.serialized")
@@ -26,7 +29,10 @@ public final class Serialized<Value> {
     }
 
 
-    /// Initialized for property wrapper. Default value is NOT inserted on synchronization queue. This initializer allows to specify custom queue name. This initializer must be overloaded. If we use default value in constructor, compiler can not syntetie default constructor for enclosing types that accepts generic type argument of Serialized.
+    /// Initializes the property wrapper. Default value is NOT inserted on synchronization queue. This initializer
+    /// allows to specify custom queue name. This initializer must be overloaded. If we use default value in
+    /// initializer, compiler can not syntetize default constructor for enclosing types that accepts generic type
+    /// argument of Serialized.
     /// - Parameters:
     ///   - wrappedValue: Default value, not inserted on synchronization queue
     ///   - label: Label for synchronization queue.
@@ -45,8 +51,12 @@ public final class Serialized<Value> {
         }
     }
 
-    /// This method dispatches it's block argument on the synchronization queue, allowing user to modify enclosed value. It is enouraged to use this method to make more complex operations with the stored property, like read-and-write. Do not perform any time-demading operations in this block since it will stop other uses of the stored property.
-    /// - Parameter transform: Opration block, argument is current value of enclosed property, return is then stored into enclosed property.
+    /// This method dispatches its block argument on the synchronization queue, allowing user to modify enclosed
+    ///  value. It is enouraged to use this method to make more complex operations with the stored property, like
+    ///  read-and-write. Do not perform any time-demading operations in this block since it will stop other uses of
+    ///  the stored property.
+    /// - Parameter transform: Opration block, argument is current value of enclosed property, return is
+    /// then stored into enclosed property.
     public func asyncAccess(transform: @escaping (Value) -> Value) {
         queue.async {
             self.value = transform(self.value)
--- Sources/FTPropertyWrappers/UserDefault.swift
+++ Sources/FTPropertyWrappers/UserDefault.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-@propertyWrapper
 /// Property wrapper for storing Codable values in UserDefaults store.
+@propertyWrapper
 public final class DefaultsStore<Value: Codable> {
 
     /// User defaults store used for this property
@@ -13,17 +13,20 @@ public final class DefaultsStore<Value: Codable> {
     /// Dencoder used to decode complex types
     private let decoder: PropertyListDecoder
 
-    /// Key for encoled property, programmer is responsible for keeping this key unique in target UserDefaults domain.
+    /// Key for encoled property, programmer is responsible for keeping this key unique in target UserDefaults
+    /// domain.
     private let key: String
 
     /// Default valure returned by enclodes property getter in case, that property is not found in the UserDefaults
     private let defaultValue: Value?
 
-    /// Initializer for user defaults property wrapper.
+    /// Initializes the user defaults property wrapper.
     /// - Parameters:
-    ///   - key: Key for encoled property, programmer is responsible for keeping this key unique in target UserDefaults domain
+    ///   - key: Key for encoled property, programmer is responsible for keeping this key unique in target
+    ///   UserDefaults domain
     ///   - defaultValue: User defaults store used for this property
-    ///   - defaults: Default valure returned by enclodes property getter in case, that property is not found in the UserDefaults
+    ///   - defaults: Default valure returned by enclodes property getter in case, that property is not found in
+    ///   the UserDefaults
     ///   - encoder: Encoder used to encode complex types
     ///   - decoder: Dencoder used to decode complex types
     public init(key: String, defaultValue: Value? = nil, defaults: UserDefaults = .standard, encoder: PropertyListEncoder = PropertyListEncoder(), decoder: PropertyListDecoder = PropertyListDecoder()) {
@@ -34,7 +37,12 @@ public final class DefaultsStore<Value: Codable> {
         self.defaultValue = defaultValue
     }
 
-    /// Getter and setter for enclosed property. Property itself is not stored in this class and is always returned and stored from/in UserDefaults. This computed property attempts to store/load type as if it was a complex propety first. In case it is not encodable/decodable, this getter attemts to retrieve it it's raw form and cast it into requested type, thus being able to store default primitive types such as int or other types such as String. If those attempts fail, default value is returned. Setter also tries to store enclosed type as encoded value, otherwise it calls default set(:forKey:) method, thus storing primitive types such as int.
+    /// Getter and setter for enclosed property. Property itself is not stored in this class and is always returned and
+    /// stored from/into UserDefaults. This computed property attempts to store/load type as if it was a complex
+    /// propety first. In case it is not encodable/decodable, this getter attemts to retrieve it it's raw form and cast it
+    /// into requested type, thus being able to store default primitive types such as int or other types such as String.
+    /// If those attempts fail, default value is returned. Setter also tries to store enclosed type as encoded value,
+    /// otherwise it calls default set(:forKey:) method, thus storing primitive types such as int.
     public var wrappedValue: Value? {
         get {
             if let data = defaults.data(forKey: key), let value = try? decoder.decode(Value.self, from: data) {
