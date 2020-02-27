31d3f61c2e31c72c7c2a402e1e1a77e0280cf7b1
Mikoláš Stuchlík <mikolas.stuchlik@thefuntasty.com>
2020-02-27 14:18:09 +0100

    Example project intial commit

---
+++ Example/FTPropertyWrappersExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FTPropertyWrappersExample
+//
+//  Created by Mikoláš Stuchlík on 27/02/2020.
+//  Copyright © 2020 Futured.app. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
